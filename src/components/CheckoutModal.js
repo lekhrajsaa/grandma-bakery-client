6a2755f9f0aacfd52b031ddc47873d6257868e2d
lekhrajsaini0 <lekhrajsaini700@gmail.com>
2023-02-14 15:00:53 +0600

    form validation added

--- src/components/CheckoutModal.js
+++ src/components/CheckoutModal.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { Button, Label, TextInput } from "flowbite-react";
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
 import ReactModal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../context/CartProvider";
@@ -35,8 +36,8 @@ const CheckoutModal = ({ modalIsOpen, setIsOpen, sendDataObj }) => {
   };
 
   //sending checkout data to server
-  const sendData = (e) => {
-    e.preventDefault();
+  const sendData = () => {
+    // e.preventDefault();
     try {
       axios
         .post(
@@ -73,6 +74,16 @@ const CheckoutModal = ({ modalIsOpen, setIsOpen, sendDataObj }) => {
     navigate("/checkout");
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    sendData();
+  };
+
   return (
     <div>
       <ReactModal
@@ -80,37 +91,55 @@ const CheckoutModal = ({ modalIsOpen, setIsOpen, sendDataObj }) => {
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         style={customStyles}
-        contentLabel="Example Modal"
+        contentLabel="Checkout Modal"
+        shouldCloseOnOverlayClick={false}
       >
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <div className="mb-2 block">
-              <Label htmlFor="email1" value="Your email" />
-            </div>
-            <TextInput
-              id="email1"
+            <label
+              htmlFor="email"
+              className="block mb-2 text-lg font-semibold text-gray-900"
+            >
+              Email
+            </label>
+            <input
+              {...register("email", { required: true })}
               type="email"
-              placeholder="johndoe@email.com"
-              required={true}
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block p-2.5 w-72 md:w-96 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-400 dark:focus:border-yellow-400"
             />
+            {errors.email && <span>This field is required</span>}
           </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="address" value="Your address" />
-            </div>
-            <TextInput
+          <div className="mb-6">
+            <label
+              htmlFor="address"
+              className="block mb-2 text-lg font-semibold text-gray-900"
+            >
+              Address
+            </label>
+            <input
+              {...register("address", { required: true })}
+              type="address"
               id="address"
-              type="text"
-              placeholder="Your address"
-              required={true}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block p-2.5 w-72 md:w-96 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-yellow-400 dark:focus:border-yellow-400"
             />
+            {errors.address && <span>Please add right address</span>}
           </div>
-          <Button color="warning" onClick={sendData}>
-            Checkout
-          </Button>
-          <Button color="warning" onClick={closeModal}>
-            Close
-          </Button>
+
+          <div className="flex justify-between">
+            <button
+              onClick={closeModal}
+              className="text-white bg-yellow-400 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-400 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-400 dark:hover:bg-yellow-400 dark:focus:ring-yellow-400"
+            >
+              close
+            </button>
+            <button
+              type="submit"
+              className="text-white bg-yellow-400 hover:bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-yellow-400 font-medium rounded-lg text-sm sm:w-auto px-5 py-2.5 text-center dark:bg-yellow-400 dark:hover:bg-yellow-400 dark:focus:ring-yellow-400"
+            >
+              Checkout
+            </button>
+          </div>
         </form>
       </ReactModal>
     </div>
